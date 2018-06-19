fed71153450efdef9f80651d94676e716821a951
Thomas Galliker <thomas@bluewin.ch>
2018-06-19 22:59:01 +0200

    Update copyright

--- ObjectDumper.NuGet/Properties/AssemblyInfo.cs
+++ ObjectDumper.NuGet/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.3")]
 [assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3-pre2")]
+[assembly: AssemblyFileVersion("1.0.3-pre3")]
