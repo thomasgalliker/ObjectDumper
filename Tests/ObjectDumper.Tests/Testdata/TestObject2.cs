a5c43225a56f8d6906fd54b1becffb81ae274c75
Thomas Galliker <thomas@bluewin.ch>
2019-02-19 07:49:41 +0100

    Extend test object with fields

--- Tests/ObjectDumper.Tests/Testdata/TestObject2.cs
+++ Tests/ObjectDumper.Tests/Testdata/TestObject2.cs
@@ -1,7 +1,11 @@
-﻿namespace My
+namespace My
 {
     public class TestObject2
     {
+        public object body;
+
+        public string name;
+
         public object Body { get; set; }
 
         public string Name { get; set; }
