4b2d613afe44d1aab6fcaf70f70894e277a7b872
Thomas Galliker <thomas@bluewin.ch>
2020-05-22 10:05:17 +0200

    Rollback change in program (program is for demo purpose only)

--- Samples/ObjectDumperSample.Netfx/Program.cs
+++ Samples/ObjectDumperSample.Netfx/Program.cs
@@ -11,11 +11,11 @@ namespace ObjectDumperSample.Netfx
         {
             var persons = new List<Person>
             {
-                new Person { Name = "John", Age = 20, PersonType = typeof(Person)},
-                new Person { Name = "Thomas", Age = 30, PersonType = typeof(Person) },
+                new Person { Name = "John", Age = 20 },
+                new Person { Name = "Thomas", Age = 30 },
             };
 
-            var personsDump = ObjectDumper.Dump(persons, new DumpOptions() { DumpStyle = DumpStyle.CSharp, CustomTypeFormatter = new Dictionary<Type, Func<Type, string>>() { { typeof(Type), o => $"typeof({o.Name})" } } });
+            var personsDump = ObjectDumper.Dump(persons, DumpStyle.CSharp);
 
             Console.WriteLine(personsDump);
             Console.ReadLine();
